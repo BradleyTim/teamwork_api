724642489fcbfa25a15fd7810ab77df253b703be
BradleyTim <timbradley94@gmail.com>
2019-11-10 10:39:00 +0300

    add routes middleware

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const app = express();
+
+dotenv.config({ path: '.env' });
+
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan(':method :url :response-time'));
+app.use(cors());
+
+app.use('/api/v1', require('./routes'));
+
+app.use('/', (req, res, next) => {
+  res.status(200).send('<h1 style="text-align: center">TEAMWORK API HOME</h1>');
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`SERVER LISTENING ON PORT ${PORT}`);
+});
